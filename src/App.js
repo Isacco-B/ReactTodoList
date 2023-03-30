1eebf17558b3240b4c6ce78b164e752b746f7099
isacco <isacco@pop-os.localdomain>
2023-03-30 20:52:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function NoteListItem(props) {
+  return <li>{props.note.description}</li>;
+}
+
+function NoteList(props) {
+  return (
+    <ul>
+      {props.notes.map((note, i) => {
+        return <NoteListItem note={note} key={i} />;
+      })}
+    </ul>
+  );
+}
+
+function NoteForm(props) {
+  const [description, setDescription] = useState("");
+  const [errorMessage, setMessage] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (description.length > 3) {
+      props.addNote(description);
+      setDescription("");
+      setMessage(null);
+    } else {
+      setMessage("Please enter more than 3 characters");
+    }
+  }
+
+  return (
+    <div>
+      {errorMessage && <p>{errorMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          name="description"
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
 
 function App() {
+  const [notes, setNotes] = useState([
+    { description: "Fai la spesa", complite: false },
+    { description: "Lava la macchina", complite: false },
+    { description: "Scrivi codice", complite: false },
+    { description: "Lava i piatti", complite: false },
+  ]);
+
+  function addNote(description) {
+    setNotes([...notes, { description: description, complite: false }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NoteForm addNote={addNote} />
+      <NoteList notes={notes} />
     </div>
   );
 }
